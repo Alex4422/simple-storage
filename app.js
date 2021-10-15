c188c550271abde40029ce771236f63d9175952a
Alex4422 <alexandregautier82@gmail.com>
2021-10-15 19:39:43 +0200

    init

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('./client/build')+'/index.html');
+});
+
+
 app.listen(port, '0.0.0.0',() => console.log('Application lancé'));
