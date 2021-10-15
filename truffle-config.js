a9cc2a622871ea4187dc1a6973e28e343a26c68f
Alex4422 <alexandregautier82@gmail.com>
2021-10-15 19:24:08 +0200

    init

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +8,17 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    ropsten: {
+      provider: () =>
+          new HDWalletProvider("solar such lumber firm inhale combine old hint neutral obscure fiber dumb", "https://ropsten.infura.io/v3/0cf118ade5ad413a8bd22c155b396750"),
+      network_id: 3,
+      confirmations: 1,
+      timeoutBlocks: 200,
+      pollingInterval: 8000,
+      skipDryRun: true,
+      networkCheckTimeout: 1000000,
+      websocket: false
     }
   }
 };
