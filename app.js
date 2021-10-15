fdce401d6d890608a09758e7f760232da78f49b9
Alex4422 <alexandregautier82@gmail.com>
2021-10-15 22:31:34 +0200

    init

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(express.static(__dirname + '/app/client'));
+//app.use(express.static(__dirname + '/app/client'));
+app.use(express.static(__dirname + '/client'));
 
 app.get('/*', function(req,res){
-    const fullPath = path.join(__dirname + '/app/client/index.html');
+    //const fullPath = path.join(__dirname + '/app/client/index.html');
+    const fullPath = path.join(__dirname + '/client/index.html');
     res.sendFile(fullPath);
 })
 
