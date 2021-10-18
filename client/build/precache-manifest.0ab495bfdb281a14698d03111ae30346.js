789190b65e3429be64fd076e4f857c6a8eca63f6
Alex4422 <alexandregautier82@gmail.com>
2021-10-18 19:25:08 +0200

    init

---
+++ client/build/precache-manifest.0ab495bfdb281a14698d03111ae30346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc21d42edc52fbc2c54e892a4ad60bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a02c05ff153ae28f01f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e266a25f6d308c553cd3",
+    "url": "/static/js/2.662f64d3.chunk.js"
+  },
+  {
+    "revision": "1a02c05ff153ae28f01f",
+    "url": "/static/js/main.6835093e.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
