3fd360646b9bc3a86abd865073adb635d0c08f0d
Alex4422 <alexandregautier82@gmail.com>
2021-10-27 18:08:04 +0200

    Updates

---
+++ precache-manifest.d6022be9e0677d023d81e6beb57a459b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa43104ea8776224099d48495772f25c",
+    "url": "/simple-storage/index.html"
+  },
+  {
+    "revision": "050f28d1ab8981a6f788",
+    "url": "/simple-storage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "25920a59fcff1e74fe00",
+    "url": "/simple-storage/static/js/2.a745f220.chunk.js"
+  },
+  {
+    "revision": "050f28d1ab8981a6f788",
+    "url": "/simple-storage/static/js/main.653eb71e.chunk.js"
+  },
+  {
+    "revision": "393911a39966e07e4fca",
+    "url": "/simple-storage/static/js/runtime-main.5a136975.js"
+  }
+]);
